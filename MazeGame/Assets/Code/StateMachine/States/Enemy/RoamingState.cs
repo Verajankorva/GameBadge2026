37b7511aec679d87b7f9e1c127bf1d0d0d0c0234
ANTTIPC\antti <antti@verajankorva.com>
2026-05-07 22:01:35 +0300

    Added load and save game.

--- MazeGame/Assets/Code/StateMachine/States/Enemy/RoamingState.cs
+++ MazeGame/Assets/Code/StateMachine/States/Enemy/RoamingState.cs
@@ -25,6 +25,8 @@ namespace StateMachine.States.Enemy
             {
                 m_isMoving = false;
             }
+            Game.m_gameData.m_enemyPosition = Game.m_enemy.m_comp.m_agent.transform.position;
+            Game.m_gameData.m_enemyDestination = Game.m_enemy.m_comp.m_agent.destination;
         }
     }
 }
