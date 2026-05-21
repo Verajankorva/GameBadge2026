67968a0ba1dd200fb3174223ba6fe44c1ddb7445
ANTTIPC\antti <antti@verajankorva.com>
2026-05-21 20:12:04 +0300

    Added custom profiler markers.

--- MazeGame/Assets/Code/StateMachine/Core/StateMachine.cs
+++ MazeGame/Assets/Code/StateMachine/Core/StateMachine.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Profiling;
 
 namespace StateMachine.Core
 {
@@ -31,14 +32,19 @@ namespace StateMachine.Core
             }
         }
 
+        static ProfilerMarker fsmParameter = new ProfilerMarker("Check parameter");
+
         public BaseParameter GetParameter(string name)
         {
             foreach(BaseParameter p in m_parameters)
             {
-                if (p.m_name.ToLower() == name.ToLower())
+                fsmParameter.Begin();
+                if (p.m_name == name)
                 {
+                    fsmParameter.End();
                     return p;
                 }
+                fsmParameter.End();
             }
             return null;
         }
@@ -53,10 +59,17 @@ namespace StateMachine.Core
             m_update = false;
         }
 
+        static ProfilerMarker fsmUpdate = new ProfilerMarker("State machine update function");
+        static ProfilerMarker fsmConditionCheck = new ProfilerMarker("State condition check");
+        static ProfilerMarker fsmUpdateState = new ProfilerMarker("State update");
+        static ProfilerMarker fsmStartState = new ProfilerMarker("State start");
+
         public void UpdateStateMachine()
         {
+            fsmUpdate.Begin();
             if (m_update)
             {
+                fsmConditionCheck.Begin();
                 foreach(BaseConnection connection in m_currentState.m_connections)
                 {
                     if (connection.Condition())
@@ -68,16 +81,23 @@ namespace StateMachine.Core
                         }
                     }
                 }
+                fsmConditionCheck.End();
+
                 if (m_previousState == m_currentState)
                 {
+                    fsmUpdateState.Begin();
                     m_currentState.UpdateState();
+                    fsmUpdateState.End();
                 }
                 else
                 {
+                    fsmStartState.Begin();
                     m_previousState = m_currentState;
                     m_currentState.StartState();
+                    fsmStartState.End();
                 }
             }
+            fsmUpdate.End();
         }
     }
 }
--- MazeGame/Assets/Code/StateMachine/States/GameplayConnection.cs
+++ MazeGame/Assets/Code/StateMachine/States/GameplayConnection.cs
@@ -1,4 +1,5 @@
 using StateMachine.Core;
+using Unity.Profiling;
 
 namespace StateMachine.States
 {
@@ -7,13 +8,20 @@ namespace StateMachine.States
         public GameplayConnection(StateMachine.Core.StateMachine fsm) : base(fsm)
         {}
 
+        static ProfilerMarker stateCondition = new ProfilerMarker("State condition");
+        static ProfilerMarker stateParameter = new ProfilerMarker("State parameter");
         public override bool Condition()
         {
+            stateCondition.Begin();
+            stateParameter.Begin();
             ParameterBool p = (ParameterBool)this.m_fsm.GetParameter("Gameplay");
+            stateParameter.End();
             if(p == null)
             {
+                stateCondition.End();
                 return false;
             }
+            stateCondition.End();
             return p.m_value;
         }
     }
